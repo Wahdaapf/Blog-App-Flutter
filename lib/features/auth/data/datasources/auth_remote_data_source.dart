29051b7379d37d889b4ebd09b678e88c4524a05f
Wahdaapf <wahdaadella.ba@gmail.com>
2026-01-14 21:51:49 +0700

    feat: sign in 02.08.44

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,49 @@
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+  AuthRemoteDataSourceImpl({required this.supabaseClient});
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        email: email,
+        password: password,
+        data: {'name': name},
+      );
+      if (response.user == null) {
+        throw ServerException('User not found');
+      }
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {} catch (e) {}
+    return email;
+  }
+}
--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -50,7 +50,7 @@ class _LoginPageState extends State<LoginPage> {
                 isObscureText: true,
               ),
               const SizedBox(height: 20),
-              AuthGradientButton(buttonText: "Sign In"),
+              AuthGradientButton(buttonText: "Sign In", onPressed: () {}),
               const SizedBox(height: 20),
               GestureDetector(
                 onTap: () {
