d0a1b32138841ffdbbd6625c909e55ae228fd300
Wahdaapf <wahdaadella.ba@gmail.com>
2026-01-17 14:25:57 +0700

    feat: login

--- lib/features/auth/domain/usecases/user_login.dart
+++ lib/features/auth/domain/usecases/user_login.dart
@@ -1,13 +1,19 @@
 import 'package:blog_app/core/error/failures.dart';
 import 'package:blog_app/core/usecase/usecase.dart';
 import 'package:blog_app/features/auth/domain/entities/user.dart';
-import 'package:fpdart/src/either.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
 
 class UserLogin implements UseCase<User, UserLoginParams> {
+  final AuthRepository authRepository;
+  const UserLogin(this.authRepository);
+
   @override
-  Future<Either<Failure, User>> call(UserLoginParams params) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, User>> call(UserLoginParams params) async {
+    return await authRepository.loginWithEmailPassword(
+      email: params.email,
+      password: params.password,
+    );
   }
 }
 
